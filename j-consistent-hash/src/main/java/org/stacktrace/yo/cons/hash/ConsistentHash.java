2f2cff7bcaef4da4e31b1efab7596bd651a21340
Ahmad Farag <ahmad.k.farag@gmail.com>
2018-10-25 21:41:33 -0400

    stuff

---
+++ j-consistent-hash/src/main/java/org/stacktrace/yo/cons/hash/ConsistentHash.java
@@ -0,0 +1,12 @@
+package org.stacktrace.yo.cons.hash;
+
+import com.google.common.hash.Hashing;
+
+public class ConsistentHash {
+
+    public static void main(String args) {
+
+//        Hashing.murmur3_128()
+
+    }
+}
